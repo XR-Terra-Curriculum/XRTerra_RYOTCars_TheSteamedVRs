226c57886c70c566673c7615dd570a3f9d33ad17
gillysuit <contemporarygil@gmail.com>
2020-11-14 17:02:14 -0800

    Add my setup scene loader files

---
+++ Assets/Editor/SceneMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class SceneMenu
+{
+    [MenuItem("Scenes/Dealership")]
+    public static void OpenDealership()
+    {
+        OpenScene("Production- Dealership");
+    }
+
+    [MenuItem("Scenes/Forest")]
+    public static void OpenForest()
+    {
+        OpenScene("Production- Forest");
+    }
+
+    [MenuItem("Scenes/Victorian")]
+    public static void OpenVictorian()
+    {
+        OpenScene("Production- Victorian");
+    }
+
+    private static void OpenScene(string sceneName)
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Production-Maps/Production- Persistent.unity", OpenSceneMode.Single);
+        EditorSceneManager.OpenScene("Assets/Scenes/Production-Maps/" + sceneName + ".unity", OpenSceneMode.Additive);
+    }
+}
