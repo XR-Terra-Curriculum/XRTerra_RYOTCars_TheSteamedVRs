b473eb516c1976ec062f78981ba2201c1cc9e1a1
asfaress <32991144+asfaress@users.noreply.github.com>
2020-11-18 16:37:08 -0700

    Teleporation & Paintball Prefabs

---
+++ Assets/Scripts/Paintball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintball : MonoBehaviour
+{
+    private Material newMaterial;
+    public GameObject carMesh;
+    public GameObject paintBall;
+    Vector3 positionToSave;
+
+    void Start()
+    {
+        newMaterial = paintBall.GetComponent<Renderer>().material;
+        positionToSave = paintBall.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            carMesh.GetComponent<Renderer>().material = newMaterial;
+        }
+        else if (other.gameObject.tag == "Hands")
+        {
+            //enable XR throw mechanic
+        }
+            
+        Destroy(paintBall);
+        Instantiate(paintBall, positionToSave, Quaternion.identity);
+    }
+}
